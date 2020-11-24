9ddb24d67fe12aae63c36eccf5930d3e4ce648cf
Sohambasu07 <soham.basu07@gmail.com>
2020-11-24 20:59:02 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from src import trainer, visualizer
+
+train_dataset, train_dataloader, spe = trainer.build_train_dataset()
+model = trainer.build_model()
+model = trainer.compile_model(model)
+visualizer(model, train_dataloader)
+trainer.train(train_dataset, model, steps_per_epoch=spe)
+visualizer(model, train_dataloader)
