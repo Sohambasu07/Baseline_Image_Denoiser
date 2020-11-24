16b8622344d5b8353477670bd32b0979696795ad
Sohambasu07 <soham.basu07@gmail.com>
2020-11-24 20:53:23 +0530

    Update base_den_config.py
    Updated configuration file with model parameters

--- base_den_config.py
+++ base_den_config.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 dataset_url = 'https://data.deepai.org/PASCALVOC2007.zip'
 file_name = 'VOCTrainVal'
 
@@ -5,3 +7,17 @@ img_size = 256
 n_ch = 3
 batch_size = 8
 buf_size = 1024
+
+epochs = 50
+
+sgd = tf.keras.optimizers.SGD
+adam = tf.keras.optimizers.Adam
+rmsp = tf.keras.optimizers.RMSprop
+pixel_mse = 'mean_squared_error'
+lr = 0.0001
+
+optimizer = adam
+loss = pixel_mse
+
+checkpoint_path = '/content/checkpoints'
+checkpoint_name = 'BaseLine_Denoiser.h5'
