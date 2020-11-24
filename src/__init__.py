0a40e1da17c22ea234876da6218feae4ee50d1fa
Sohambasu07 <soham.basu07@gmail.com>
2020-11-24 20:58:59 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .model import DenoiseNet
 from .dataloader import DataLoader
 from .trainer import build_train_dataset, build_model, compile_model, train
+from .model_visualizer import visualizer
