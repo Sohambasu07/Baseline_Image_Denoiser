c45a01c90f983a70600956f070ef173ca2484c80
Sohambasu07 <soham.basu07@gmail.com>
2023-04-09 15:13:20 +0200

    Update dataset_downloader.py

--- src/dataloader/dataset_downloader.py
+++ src/dataloader/dataset_downloader.py
@@ -1,11 +1,16 @@
 import zipfile
 import tensorflow as tf
 import base_den_config as cfg
+import zipfile
 
 def download():
     file_name = cfg.file_name
     dataset_url = cfg.dataset_url
-    dataset_path = tf.keras.utils.get_file(
+    dataset_zip_path = tf.keras.utils.get_file(
             file_name, dataset_url, extract=True
         )
+    dataset_path = '/content/Dataset'
+    zip_ref = zipfile.ZipFile(dataset_zip_path, 'r')
+    zip_ref.extractall(dataset_path)
+    zip_ref.close()
     return dataset_path
