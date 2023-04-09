8fa40fedf6b40b9fb925100ce26fa77ff8def783
Sohambasu07 <soham.basu07@gmail.com>
2023-04-09 15:43:32 +0200

    Update dataset_downloader.py

--- src/dataloader/dataset_downloader.py
+++ src/dataloader/dataset_downloader.py
@@ -7,7 +7,7 @@ def download():
     file_name = cfg.file_name
     dataset_url = cfg.dataset_url
     dataset_zip_path = tf.keras.utils.get_file(
-        fname = '/content/'+file_name, origin = dataset_url)#, extract=True)
+        fname = '/content/Dataset.tgz', origin = dataset_url)#, extract=True)
     dataset_path = '/content/Dataset'
     zip_ref = tarfile.open(dataset_zip_path, 'r')
     zip_ref.extractall(dataset_path)
